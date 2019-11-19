4107d3c96915cf5a1a4612ed1e202110f1a41bfb
bodhicougar <info@blackboxmd.fyi>
2019-11-19 19:25:48 +0100

    Netlify CMS versucht einzubinden

--- src/Pages/Blogs.js
+++ src/Pages/Blogs.js
@@ -1,54 +1,70 @@
-import React, { Component } from 'react'
-import client from '../Pages/Client'
-import BlackLoader from '../images/black-loader.gif'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import client from '../Pages/Client';
+import BlackLoader from '../images/black-loader.gif';
+import { Link } from 'react-router-dom';
+import CMS from 'netlify-cms';
 
 class Blogs extends Component {
-    constructor() {
-        super()
-        this.state = { blogpage: [] }
-    }
-    componentDidMount() {
-        client.getEntries({
-            content_type: 'blogs'
-        }).then((entries) => {
-            this.setState({ blogpage: entries.items })
-        })
-    }
-    render() {
-        return (
-            <div>
-                <section className="pt-4">
-                    <div className="container">
-                        <div className="text-center">
-                            <h2> Blogs </h2>
-                            <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta in ipsum non facilisis.</p>
-                        </div>
-                        {this.state.blogpage.length === 0 ?
-
-                            <div align="center" className="pt-5"> <img src={BlackLoader} alt="Loader" /> </div>
-                            :
-                            <div className="row">
-                            {this.state.blogpage.map((item, index) => {
-                                return (
-                                    <div key={index} className="col-md-6 blog-content">
-                                        <img src={item.fields.blogThumbnail.fields.file.url} className="img-blog img-fluid" alt="" />
-                                        <h3><Link to={`../Blogs/${item.fields.slug}`}>{item.fields.blogTitle}</Link></h3>
-                                        <p>{item.fields.blogDescription}</p>
-                                        <button className="btn btn-primary"><Link to={`../Blogs/${item.fields.slug}`}>Read more...</Link></button>
-                                    </div>
-                                )
-                            }
-
-                            )}
-                        </div>
-
-                        }
-                        
-                    </div>
-                </section>
+  constructor() {
+    super();
+    this.state = { blogpage: [] };
+  }
+  componentDidMount() {
+    client
+      .getEntries({
+        content_type: 'blogs'
+      })
+      .then(entries => {
+        this.setState({ blogpage: entries.items });
+      });
+  }
+  render() {
+    return (
+      <div>
+        <section className="pt-4">
+          <div className="container">
+            <div className="text-center">
+              <h2> Blogs </h2>
+              <p className="lead">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Curabitur porta in ipsum non facilisis.
+              </p>
             </div>
-        );
-    }
+            {this.state.blogpage.length === 0 ? (
+              <div align="center" className="pt-5">
+                {' '}
+                <img src={BlackLoader} alt="Loader" />{' '}
+              </div>
+            ) : (
+              <div className="row">
+                {this.state.blogpage.map((item, index) => {
+                  return (
+                    <div key={index} className="col-md-6 blog-content">
+                      <img
+                        src={item.fields.blogThumbnail.fields.file.url}
+                        className="img-blog img-fluid"
+                        alt=""
+                      />
+                      <h3>
+                        <Link to={`../Blogs/${item.fields.slug}`}>
+                          {item.fields.blogTitle}
+                        </Link>
+                      </h3>
+                      <p>{item.fields.blogDescription}</p>
+                      <button className="btn btn-primary">
+                        <Link to={`../Blogs/${item.fields.slug}`}>
+                          Read more...
+                        </Link>
+                      </button>
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
-export default Blogs
+export default Blogs;
--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -1,59 +1,69 @@
-import React, { Component } from 'react'
-import client from '../Pages/Client'
-import loader from '../images/loader.gif'
+import React, { Component } from 'react';
+import client from '../Pages/Client';
+import loader from '../images/loader.gif';
+import CMS from 'netlify-cms';
 
 class Services extends Component {
-    constructor() {
-        super()
-        this.state = { servicepage: [] }
-    }
+  constructor() {
+    super();
+    this.state = { servicepage: [] };
+  }
 
-    componentDidMount() {
-        client.getEntries({
-            'content_type': 'services', order: 'sys.createdAt'
-        }).then((entries) => {
-            this.setState({ servicepage: entries.items })
-        })
-
-    }
-    render() {
-
-        return (
-            <div>
-                <section id="services" className="p-4">
-                    <div className="container">
-                        <div className="text-center">
-                            <h2>Our Services</h2>
-                            <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta in ipsum non facilisis.</p>
-                        </div>
-                        {this.state.servicepage.length === 0 ?
-                            <div align="center"><img src={loader} alt="loader" /></div>
-                            :
-                            <div className="row">
-
-                                {this.state.servicepage.map((item, index) => {
-
-                                    return (
-                                        <div key={index} className="col-md-4">
-                                            <div className="media service-wrap">
-                                                <div>
-                                                    <img className="pr-3" src={item.fields.serviceIcon.fields.file.url} alt="" />
-                                                </div>
-                                                <div className="media-body">
-                                                    <h3 className="media-heading">{item.fields.serviceTitle}</h3>
-                                                    <p>{item.fields.serviceDescription}</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    )
-                                }
-                                )}
-                            </div>
-                        }
-                    </div>
-                </section>
+  componentDidMount() {
+    client
+      .getEntries({
+        content_type: 'services',
+        order: 'sys.createdAt'
+      })
+      .then(entries => {
+        this.setState({ servicepage: entries.items });
+      });
+  }
+  render() {
+    return (
+      <div>
+        <section id="services" className="p-4">
+          <div className="container">
+            <div className="text-center">
+              <h2>Our Services</h2>
+              <p className="lead">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Curabitur porta in ipsum non facilisis.
+              </p>
             </div>
-        );
-    }
+            {this.state.servicepage.length === 0 ? (
+              <div align="center">
+                <img src={loader} alt="loader" />
+              </div>
+            ) : (
+              <div className="row">
+                {this.state.servicepage.map((item, index) => {
+                  return (
+                    <div key={index} className="col-md-4">
+                      <div className="media service-wrap">
+                        <div>
+                          <img
+                            className="pr-3"
+                            src={item.fields.serviceIcon.fields.file.url}
+                            alt=""
+                          />
+                        </div>
+                        <div className="media-body">
+                          <h3 className="media-heading">
+                            {item.fields.serviceTitle}
+                          </h3>
+                          <p>{item.fields.serviceDescription}</p>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
-export default Services
+export default Services;
